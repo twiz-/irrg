3b3a4e79811aec1653c79213fefea1440cc8d6c1
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-05-28 12:06:27 -0700

    move os storage to gitignore, and better naming for savied img files

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,14 +17,6 @@ class ImageUploader < CarrierWave::Uploader::Base
     "merchants/#{model.class.to_s.underscore}s/#{mounted_as}/#{model.location}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
@@ -39,14 +31,20 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def filename
+    "#{secure_token}+#{model.location}.#{file.extension}" if original_filename
+  end
+  
+  protected
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(10))
+  end
 
 end
