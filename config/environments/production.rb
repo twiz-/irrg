f126c56bea4e174853151551feb678feb64b7145
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-05-31 11:50:08 -0700

    appease heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
+  
+  config.assets.initialize_on_precompile = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
