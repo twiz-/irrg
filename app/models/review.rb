c1f73b6b37b40e18dedcd885c23be4a159fb15a1
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-06-04 14:36:08 -0700

    fix styling for heroku, add validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,9 @@ class Review < ActiveRecord::Base
   
   belongs_to :location
   
+  validates :product_image,:email, :first_name,:location_id,:comment,:reviewer_image, presence: true
+  validates :comment, length: { in: 6..200}
+  
   mount_uploader :product_image, ImageUploader
   mount_uploader :reviewer_image, ImageUploader
   
