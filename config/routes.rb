779324a2bd12aaf6b4db0454df24a7626a9d13d9
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-06-10 10:29:14 -0700

    Add devise views

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do 
     get "/login" => 'devise/sessions#new'
+    get "/signup" => 'devise/registrations#new'
   end
   
   root to: 'reviews#new'
