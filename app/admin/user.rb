f38ef672b38cdcd7b4467e828e10294fe58c7bf6
Tony Ramirez <tony.michaelramirez@gmail.com>
2014-06-12 17:32:52 -0700

    sanitize updating through ActiveAdmin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,10 +1,10 @@
 ActiveAdmin.register User do
+  permit_params :approved
 
   index do 
     column :email
     column :last_sign_in_at
     column :approved
-    
+    actions
   end
-  
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
   
@@ -12,4 +13,10 @@ class ApplicationController < ActionController::Base
       admin_dashboard_path(resource)
     end
   end  
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:email, :password, :password_confirmation, :approved)
+    end
+  end
 end
